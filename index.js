b15cdf326fa0b7bb517b9b50ccbd08852c295770
Aliasgar Pocketwala <pocketwala.ali@gmail.com>
2021-02-05 13:07:16 +0530

    -a

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   //   res.sendFile(__dirname + "/index.html");
-  console.log("Hello World");
+  res.send("Hello World");
 });
 
 io.on("connection", (socket) => {
