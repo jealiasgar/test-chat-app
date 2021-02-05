fee59078dd0bb8e90d3edb3770e39f4bfed18fbe
Aliasgar Pocketwala <pocketwala.ali@gmail.com>
2021-02-05 13:17:25 +0530

    -a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   //   res.sendFile(__dirname + "/index.html");
@@ -14,6 +14,11 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+// http.listen(port, () => {
+//   console.log(`Socket.IO server running at http://localhost:${port}/`);
+// });
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
